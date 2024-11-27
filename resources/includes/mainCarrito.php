41c2dd5ec9f4de35ca4886477f038224eb373834
Lubriom <lunarmorer@gmail.com>
2024-11-27 01:50:54 +0100

    Se sube actualización de pagina carrito

---
+++ resources/includes/mainCarrito.php
@@ -0,0 +1,34 @@
+<div class="main__carrito">
+    <div class="productos">
+        <div class="producto">
+            <img src="https://th.bing.com/th/id/OIP.L2t67oR-hYV4Kpxt7eg9mAHaHa?rs=1&pid=ImgDetMain" alt="">
+            <p>Coche de Juguete</p>
+            <p>12,99€</p>
+            <input type="button" class="button" value="Eliminar">
+            </input>
+        </div>
+        <div class="producto">
+            <img src="https://th.bing.com/th/id/OIP.L2t67oR-hYV4Kpxt7eg9mAHaHa?rs=1&pid=ImgDetMain" alt="">
+            <p>Coche de Juguete</p>
+            <p>12,99€</p>
+            <input type="button" class="button" value="Eliminar">
+            </input>
+        </div>
+        <div class="producto">
+            <img src="https://th.bing.com/th/id/OIP.L2t67oR-hYV4Kpxt7eg9mAHaHa?rs=1&pid=ImgDetMain" alt="">
+            <p>Coche de Juguete</p>
+            <p>12,99€</p>
+            <input type="button" class="button" value="Eliminar">
+            </input>
+        </div>
+    </div>
+    <div class="opciones">
+        <div class="control">
+            <p>Cantidad de productos: 1</p>
+            <p>Precio Total: 12,99€</p>
+        </div>
+        <div class="botones">
+            <input type="button" class="button" value="Vaciar Carrito"></input>
+        </div>
+    </div>
+</div>
